0214e35976665ea2f211b562860884c69f9125cd
sseezov <sseezov@mail.ru>
2024-03-10 15:28:11 +0700

    added shooting

--- index.js
+++ index.js
@@ -20,8 +20,50 @@ class Player {
   }
 }
 
+class Projectile {
+  constructor(x, y, radius, color, velocity){
+    this.x = x
+    this.y = y
+    this.radius = radius
+    this.color = color
+    this.velocity = velocity
+  }
+  draw() {
+    context.beginPath();
+    context.arc(this.x, this.y, this.radius, 0, Math.PI * 2)
+    context.fillStyle = this.color
+    context.fill()
+  }
+  update() {
+    this.draw()
+    this.x = this.x + this.velocity.x
+    this.y = this.y + this.velocity.y
+  }
+}
+
 const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'blue');
-player.draw()
+
+const projectiles = []
+
+const animate = () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  requestAnimationFrame(animate);
+  projectiles.forEach((proj) => {
+    proj.update()
+  })
+  
+}
+
+window.addEventListener('click', (e)=> {
+  const angle = Math.atan2(e.clientY - y, e.clientX - x);
+  const velocity = {
+    x: Math.cos(angle),
+    y: Math.sin(angle)
+  }
+  projectiles.push(new Projectile(x, y, 5, 'black', velocity))
+  animate()
+})
